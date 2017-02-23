aa161f5cb2366e7fe558a96b41273c9b2a4847c2
nansyg21 <it12148@uom.edu.gr>
2017-02-23 20:56:15 +0200

    main

--- src/hashcode2017/Main.java
+++ src/hashcode2017/Main.java
@@ -13,19 +13,33 @@ public class Main {
 		OutputParser op=new OutputParser();
 		
 		
-		ip.readFile("me_at_the_zoo.txt");
+		ip.readFile("me_at_the_zoo.in");
 		System.out.print("first "+ip.firstLine +"\n");
-
+		
+		ip.firstLineSplit(ip.firstLine);
+		int howManyVids=Integer.parseInt(ip.firstLineSplit[0]);
+		System.out.println("Vids "+howManyVids);
+		int howManyEndpoints=Integer.parseInt(ip.firstLineSplit[1]);
+		System.out.println("End points "+howManyEndpoints);
+		int howManyRequests=Integer.parseInt(ip.firstLineSplit[2]);
+		System.out.println("Requests "+howManyRequests);
+		int howManyCaches=Integer.parseInt(ip.firstLineSplit[3]);
+		System.out.println("Caches "+howManyCaches);
+		int capacityPerCache=Integer.parseInt(ip.firstLineSplit[4]);
+		System.out.println("Capacity "+howManyVids);
+		
 		String[] splitLine=ip.videoLine.split("\\s+");
-		System.out.print("second line length "+splitLine.length +"\n");
+		//int howManyVids=splitLine.length;
+		
+		//System.out.print("second line length "+splitLine.length +"\n");
 		
 		/*for(int i=0;i<ip.fileLines.size();i++)
 		{
 			System.out.println(ip.fileLines.get(i)+" -line: "+i);
 			
 		}*/
-	//	op.Write("newOutput.txt", ip.fileLines);
-	//	op.Append("newOutput.txt", "append");
+		op.Write("newOutput.txt", ip.fileLines);
+		op.Append("newOutput.txt", "append");
 	}
 	
 
