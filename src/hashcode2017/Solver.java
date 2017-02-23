ebeae2f1df7ea0dcbdf85232d64a4873ceaaecb4
panagioths-k <panos_kotsikoris@hotmail.com>
2017-02-23 21:36:38 +0200

    empty solver

---
+++ src/hashcode2017/Solver.java
@@ -0,0 +1,28 @@
+package hashcode2017;
+
+import java.util.ArrayList;
+
+public class Solver {
+	
+	ArrayList<Video> videos;
+	ArrayList<Request> requests;
+	ArrayList<Cache> caches;
+	ArrayList<Cache> endpoints;
+	
+	
+	public Solver(ArrayList<Video> _videos, ArrayList<Request> _requests, ArrayList<Cache> _caches, ArrayList<Cache> _endpoints )
+	{
+		videos = _videos;
+		requests = _requests;
+		caches = _caches;
+		endpoints = _endpoints;		
+	}
+	
+	public void Solve()
+	{
+		
+		
+		
+	}
+
+}
